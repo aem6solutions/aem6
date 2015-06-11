3a50e4135442659dbf045bd0800c3b18a9442706
anshika dhingra <anshika@intelligrape.com>
2015-06-11 18:00:57 +0530

    Initial Project Setup

---
+++ core/src/main/java/com/aem6solutions/project/core/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.aem6solutions.project.core;
+
+import aQute.bnd.annotation.Version;
---
+++ core/src/test/java/com/aem6solutions/project/core/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.aem6solutions.project.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
